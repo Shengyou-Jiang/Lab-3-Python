f0412825fae19a6c1b4437546a613175d6bae549
Shengyou Jiang <sjj5492@psu.edu>
2020-09-15 20:28:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Author: Shengyou Jiang sjj5492@psu.edu
+# Collaborator: Kyle Domico kdd5262@psu.edu
+# Collaborator: Teya Davis tmd5681@psu.edu
+# Collaborator: Travis Stauffer tcs5399@psu.edu
+# Section: 1
+# Breakout: 2
+
+def sum_n(n):
+  if n <= 1:
+    return n
+  else:
+    return n + sum_n(n-1)
+  
+
+def print_n(s,n):
+  if n == 0:
+    return 
+  else:
+    print_n(s, n-1)
+    print(s)
+  
+
+def run():
+  num1 = int(input("Enter an int: "))
+  print(f"sum is {sum_n(num1)}")
+  s = input("Enter a string: ")
+  print_n(s, num1)
+
+
+__name__ == "__main__"
+run ()
